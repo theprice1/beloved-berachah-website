d91a3abcf031334e31dd514954e822bda1ae0e44
theprice1 <66714035+theprice1@users.noreply.github.com>
2026-09-06 09:54:36 +0100

    Finalize index.html prototype with PWA support, mobile menu, and legal links

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'berachah-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/style.css',
+  '/assets/images/BB.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
