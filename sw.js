0d135bae72b4319b3b26071263129ec8efeb8af5
theprice1 <66714035+theprice1@users.noreply.github.com>
2026-09-12 17:03:16 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,33 @@
-const CACHE_NAME = 'berachah-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/assets/images/BB.png',
-  './manifest.json'
+const CACHE_NAME = 'beloved-berachah-v2'; // The version bump is critical
+
+const ASSETS = [
+  '/BelovedBerachah/',
+  '/BelovedBerachah/index.html',
+  '/BelovedBerachah/css/style.css',
+  '/BelovedBerachah/assets/images/BB.png',
+  '/BelovedBerachah/manifest.json'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
 });
